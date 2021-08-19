9cb80ad4b7ef4124ba98141b57f4359a0b0506c5
SergioRius <sergio.rius@hotmail.com>
2021-08-19 19:32:42 +0200

    Fix victron node name and make it optional

--- setup.sh
+++ setup.sh
@@ -5,12 +5,21 @@
 # Author:  Sergio Rius
 #
 
-echo "Installed required node modules..."
-docker exec -it nodered /bin/bash -c "cd /data && npm install node-red-dashboard node-red-contrib-uibuilder node-red-contrib-influxdb node-red-contrib-victron node-red-contrib-buffer-parser node-red-contrib-modbus node-red-contrib-socketio"
-docker restart nodered
+echo "Installing required node modules..."
+docker exec nodered /bin/bash -c "cd /data && npm i node-red-dashboard node-red-contrib-uibuilder node-red-contrib-influxdb node-red-contrib-buffer-parser node-red-contrib-modbus node-red-contrib-socketio node-red-contrib-watt2kwh"
+
+echo;
+read -p "Do you wish to install the victron nodes? (y/n) " yn
+case $yn in
+  [Yy]* )
+    docker exec nodered /bin/bash -c "cd /data && npm i @victronenergy/node-red-contrib-victron"
+  ;;
+esac
 
 echo "Importing node-red flows..."
 for f in nodered/autoimport/*.json; do
   echo "$f"
   curl -X POST http://localhost:1880/flow -H 'content-type: application/json' --data "@$f"
 done
+
+docker restart nodered
